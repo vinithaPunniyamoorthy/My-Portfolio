2c6cf767f98aabe54c838baf4deab8fa59417cf1
vinithaPunniyamoorthy <162816259+vinithaPunniyamoorthy@users.noreply.github.com>
2025-01-27 13:07:57 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <footer className='w-full py-[60px]'>
+        <div className='flex justify-center items-center h-[220px]'>
+            <p className="font-bad-script text-3xl md:text-4xl lg:text-5xl text-center"><span className='text-primaryBlue'>“</span> Thanks for scrolling! <span className='text-primaryBlue'>”</span></p>
+        </div>
+        <p className='text-sm md:text-lg text-center'>Crafted by Dinuranga &copy; 2024.</p>
+    </footer>
+  )
+}
+
+export default footer
